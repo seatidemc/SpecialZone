f8fc6b1408bbe722d48ebaad46b35e6b8562c23b
Subilan <suyuanliang1995@gmail.com>
2021-10-01 21:02:06 +0800

    fix, add `claim`

--- src/main/java/top/seatide/SpecialZone/Commands.java
+++ src/main/java/top/seatide/SpecialZone/Commands.java
@@ -31,8 +31,9 @@ public class Commands implements TabExecutor {
     }
 
     public Commands() {
-        supportedProperties = Arrays.asList("keepInv", "keepExp", "noBreak", "ignoreY");
-        zoneNames = Arrays.asList((String[]) Files.zones.getKeys(false).toArray());
+        supportedProperties = Arrays.asList("keepInv", "keepExp", "noBreak", "ignoreY", "noPlace", "noIgnite",
+                "noContainer");
+        zoneNames = new ArrayList<>(Files.zones.getKeys(false));
     }
 
     @Override
@@ -43,17 +44,20 @@ public class Commands implements TabExecutor {
                 return getResult(args[0], Arrays.asList(ARGS_1ST));
             }
             if (args.length == 2) {
-                if (args[0] == "set" || args[0] == "delete" || args[0].endsWith("ex")) {
+                if (args[0].equals("set") || args[0].equals("delete") || args[0].endsWith("ex")) {
                     return getResult(args[1], zoneNames);
                 }
             }
             if (args.length == 3) {
-                if (args[0] == "set" || args[0] == "delex" || args[0] == "setex") {
+                if (args[0].equals("set") || args[0].equals("delex") || args[0].equals("setex")) {
                     return getResult(args[2], supportedProperties);
+                }
+                if (args[0].equals("claim")) {
+                    return getResult(args[2], Arrays.asList(BOOLEAN_OPTIONS));
                 }
             }
             if (args.length == 4) {
-                if (args[0] == "set") {
+                if (args[0].equals("set")) {
                     return getResult(args[3], Arrays.asList(BOOLEAN_OPTIONS));
                 }
             }
@@ -64,6 +68,10 @@ public class Commands implements TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("specialzone")) {
+            if (args.length == 0) {
+                LogUtil.send(sender, "参数不足。");
+                return true;
+            }
             switch (args[0]) {
                 case "create": {
                     if (!(sender instanceof Player)) {
@@ -75,13 +83,13 @@ public class Commands implements TabExecutor {
                         return true;
                     }
                     if (Zone.contains(args[1])) {
-                        LogUtil.send(sender, "&c创建区域失败，区域已存在。");
+                        LogUtil.send(sender, "&c创建区域失败，同名区域已存在。");
                         return true;
                     }
                     Player p = (Player) sender;
                     UUID u = p.getUniqueId();
                     var result = Events.selectionState.get(u);
-                    if (result.equals(null)) {
+                    if (result == null) {
                         LogUtil.send(sender, "&c创建区域失败，请先使用&e箭&c选择区域的范围。");
                         return true;
                     }
@@ -96,6 +104,8 @@ public class Commands implements TabExecutor {
                     var zone = new Zone(args[1], result[0], result[1]);
                     zone.create();
                     LogUtil.send(sender, "&a成功创建区域！");
+                    Events.selectionState.remove(u);
+                    Files.reload();
                     break;
                 }
 
@@ -117,6 +127,33 @@ public class Commands implements TabExecutor {
                     }
                     Zone.delete(name);
                     LogUtil.send(sender, "成功删除区域 &c" + name + "&r。");
+                    Files.reload();
+                    break;
+                }
+
+                case "claim": {
+                    if (args.length < 2) {
+                        LogUtil.send(sender, "&c参数不足。");
+                        return true;
+                    }
+                    var name = args[1];
+                    if (!Zone.contains(name)) {
+                        LogUtil.send(sender, "&c区域不存在。");
+                        return true;
+                    }
+                    var zone = new Zone(name);
+                    var value = true;
+                    if (args.length >= 3)
+                        value = Boolean.parseBoolean(args[2]);
+                    zone.setProperty("noBreak", value);
+                    zone.setProperty("noPlace", value);
+                    zone.setProperty("noContainer", value);
+                    zone.setProperty("noIgnite", value);
+                    if (value) {
+                        LogUtil.send(sender, "成功将区域 &e" + name + "&r 标记为领地。");
+                    } else {
+                        LogUtil.send(sender, "成功取消标记为领地。");
+                    }
                     break;
                 }
 
@@ -142,6 +179,7 @@ public class Commands implements TabExecutor {
                     zone.setProperty(property, value);
                     LogUtil.send(sender, "成功将区域 &e" + name + "&r 的 &a" + property + "&r 属性设置为 &"
                             + (value ? "atrue" : "cfalse") + "&r。");
+                    Files.reload();
                     break;
                 }
 
@@ -150,7 +188,7 @@ public class Commands implements TabExecutor {
                         LogUtil.send(sender, "&c参数不足。");
                         return true;
                     }
-                    
+
                     var zoneName = args[1];
                     var property = args[2];
                     var player = args[3];
@@ -160,10 +198,12 @@ public class Commands implements TabExecutor {
                     }
                     var zone = new Zone(zoneName);
                     if (zone.setException(property, player)) {
-                        LogUtil.send(sender, "成功将 &a" + player + "&r 加入区域 &e" + zoneName + "&r 的 &b" + property + "&r 属性排除项。");
+                        LogUtil.send(sender,
+                                "成功将 &a" + player + "&r 加入区域 &e" + zoneName + "&r 的 &b" + property + "&r 属性排除项。");
                     } else {
                         LogUtil.send(sender, "添加排除项失败，玩家已存在。");
                     }
+                    Files.reload();
                     break;
                 }
 
@@ -181,10 +221,12 @@ public class Commands implements TabExecutor {
                     }
                     var zone = new Zone(zoneName);
                     if (zone.delException(property, player)) {
-                        LogUtil.send(sender, "成功从区域 &e" + zoneName + "&r 的 &b" + property + "&r 属性排除项移除 &a" + player + "&r。");
+                        LogUtil.send(sender,
+                                "成功从区域 &e" + zoneName + "&r 的 &b" + property + "&r 属性排除项移除 &a" + player + "&r。");
                     } else {
                         LogUtil.send(sender, "删除排除项失败，玩家不存在。");
                     }
+                    Files.reload();
                     break;
                 }
 
@@ -205,8 +247,10 @@ public class Commands implements TabExecutor {
                         LogUtil.send(sender, "&e" + zoneName + "&r 没有 &b" + property + "&r 属性的排除项。");
                     } else {
                         LogUtil.send(sender, "&e" + zoneName + "&r 的 &b" + property + "&r 属性排除项：");
-                        LogUtil.log(StringUtils.join(result, "、"));
+                        LogUtil.send(sender, "&e" + StringUtils.join(result, '、'));
                     }
+                    Files.reload();
+                    break;
                 }
 
                 default: {
